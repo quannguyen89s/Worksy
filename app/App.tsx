0bd1fb13972901250cbfaefa5a1655a6d2090376
diethon <vietthde180482@fpt.edu.vn>
2026-03-17 15:46:01 +0700

    first init reactnative app

---
+++ app/App.tsx
@@ -0,0 +1,13 @@
+import './global.css';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+  <View>
+    <Text>
+      Hello World
+    </Text>
+  </View>
+  );
+}
